fee29211ce13cee2903f2372245287b000221351
Caroline Manson <carolinemanson15@gmail.com>
2020-12-17 12:59:57 -0500

    fixed confirm()

--- script.js
+++ script.js
@@ -6,40 +6,33 @@ var characterLength = prompt("How many characters would you like your password t
 console.log(characterLength)
 
 var passNumbers = confirm("Click OK to include numbers.")
-if (true === true) {
+if (passNumbers === true) {
   pswd.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-}
+} 
 console.log(pswd)
 
 var passUppercase = confirm("Click OK to include uppercase letters.")
-if (true === true) {
+if (passUppercase === true) {
   pswd.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 }
 console.log(pswd)
 
 var passLowercase = confirm("Click OK to include lowercase letters.")
-if (true === true) {
+if (passLowercase === true) {
   pswd.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 }
 console.log(pswd)
 
 var passSpecialCh = confirm("Click OK to include special characters.")
-if (true === true) {
+if (passSpecialCh === true) {
   pswd.push("~", "!", "@", "#", "%", "^", "&", "*", "(", ")")
 }
 console.log(pswd)
 
-// var i;
-
-
-// for (i = 0; i < pswd.length; i++) {
-//   text += pswd[i];
-// }
-
 // Randomly sorting through the pswd array using Fisher Yates method
 
-// pswd.length = parseInt(characterLength);
-// console.log(pswd);
+pswd.length = parseInt(characterLength);
+console.log(pswd);
 var text = ""
 
 for (i = pswd.length -1; i > 0; i--) {
